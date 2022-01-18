b5a6743e036a654bcbe8c000ab02b57be8a0c6bc
Cleyton Rodrigues <88549697+cleytonsilvadev@users.noreply.github.com>
2022-01-18 15:49:45 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+    
+    int valor1 = 10000;
+    int valor2 = 102;
+    int soma;
+    int subt;
+    int mult;
+    double div;
+    double rest;
+
+   soma = valor1+valor2;
+   subt = valor1-valor2;
+   mult = valor1*valor2;
+   div = valor1/ valor2;
+   rest = valor1%valor2;
+    
+    
+    System.out.println(" soma= "+soma+"\n subtração= "+subt+"\n multiplicação= "+mult+"\n divisao= "+div+"\n resto= "+rest);
+
+  }
+}
