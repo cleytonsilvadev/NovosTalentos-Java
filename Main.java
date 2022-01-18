7cbc0b3b7018384cea490b4fe81707fad3b1beaf
Cleyton Rodrigues <88549697+cleytonsilvadev@users.noreply.github.com>
2022-01-18 15:53:50 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+ import java.util.Scanner;
+
+class Main {
+ 
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+  
+
+System.out.println("digite o primeiro valor");
+double nota1 = sc.nextDouble();
+System.out.println("digite o segunda valor");
+double nota2 = sc.nextDouble();
+System.out.println("digite terceira valor");
+double nota3 = sc.nextDouble();
+System.out.println("digite o quarta valor");
+double nota4 = sc.nextDouble();
+
+double media=  (nota1+nota2+nota3+nota4)/4;
+    
+    
+    System.out.println("a media do aluno é :"+media);
+
+    
+
+  }
+}
